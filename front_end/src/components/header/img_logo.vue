c8a4432384aa1d0c595aae2cb0286074f91e093f
iksadNorth <iksadnorth@gmail.com>
2022-11-13 11:54:25 +0900

    #5 - 로고 이미지 제작 1. 브랜드 이미지를 넣고 클릭시 홈페이지에 들어갈 수 있도록 메서드를 생성함. 2. 최대 높이와 최대 너비를 설정함.

---
+++ front_end/src/components/header/img_logo.vue
@@ -0,0 +1,25 @@
+<template>
+    <img 
+        class="clickable"
+        src="https://blog.kakaocdn.net/dn/6ui8u/btqDtzRrCzg/ZM8q87L5frjEl0za6UnIk1/img.jpg" 
+        @click="onClickLogo"
+    />
+</template>
+
+<script>
+export default {
+    methods: {
+        onClickLogo: function() {
+            this.$router.push(this.$endPoint.home);
+        },
+    }
+}
+</script>
+
+<style scoped>
+    img {
+        max-width: 200px;
+        max-height: 80%;
+        object-fit: contain;
+    }
+</style>

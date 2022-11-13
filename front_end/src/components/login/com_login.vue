784921c9110b6479a8d0ba27921a9cbbb35c33f2
iksadNorth <iksadnorth@gmail.com>
2022-11-13 18:52:16 +0900

    #8 - view_login.vue 설계 1. 로그인 컴포넌트 com_login.vue의 위치를 중앙으로 계획함. 2. 로그인 과정 중 문제가 생기면 아이디와 비밀번호 텍스트 밑에 관련 에러 메세지를 띄울 수 있게 설계.

---
+++ front_end/src/components/login/com_login.vue
@@ -0,0 +1,107 @@
+<template>
+    <v-container class="child-size-limit child-x-center">
+        <v-row>
+            <img_logo />
+        </v-row>
+        <v-row>
+            <v-text-field
+                clearable variant="outlined"
+                :prepend-icon="email.icon" persistent-hint
+                v-model="email.value"
+                :label="email.label"
+                :hint="email.hint"
+            ></v-text-field>
+        </v-row>
+        <v-row>
+            <v-text-field
+                clearable variant="outlined"
+                :prepend-icon="password.icon" persistent-hint type="password"
+                v-model="password.value"
+                :label="password.label"
+                :hint="password.hint"
+            ></v-text-field>
+        </v-row>
+        <v-row>
+            <v-checkbox
+                v-model="autoLogin.value"
+                :label="autoLogin.label"
+                color="primary"
+                hide-details
+            ></v-checkbox>
+        </v-row>
+        <v-row>
+            <v-btn
+                block color="primary"
+                @click="btnLogin.click"
+            >{{ btnLogin.label }}</v-btn>
+        </v-row>
+        <v-divider class="my-6" />
+        <v-row>
+            <v-btn
+                block color="primary"
+                @click="btnJoin.click"
+            >{{ btnJoin.label }}</v-btn>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+data() {return {
+    email: {
+        label: "아이디(이메일)",
+        value: undefined,
+        icon: "mdi-account",
+        hint: "",
+    },
+    password: {
+        label: "비밀번호",
+        value: undefined,
+        icon: "mdi-lock",
+        hint: "",
+    },
+    autoLogin: {
+        label: "자동 로그인",
+        value: undefined,
+    },
+
+    btnLogin: {
+        label: "로그인",
+        click: this.onClickLogin,
+    },
+    btnJoin: {
+        label: "회원가입",
+        click: this.onClickJoin,
+    },
+
+}},
+methods: {
+    fetchLogin(email, password, isAuto) {
+        email, password, isAuto
+    },
+    clearHint() {
+        const nullValue = "";
+
+        this.email.hint = nullValue;
+        this.password.hint = nullValue;
+    },
+
+    onClickLogin() {
+        console.log("click onClickLogin");
+        this.fetchLogin(this.email.value, this.password.value, this.autoLogin.value);
+    },
+    onClickJoin() {
+        console.log("click onClickJoin");
+        this.$router.push(this.$endPoint.join);
+    },
+},
+}
+</script>
+
+<style scoped>
+.child-size-limit > * {
+    width: 400px;
+
+    margin: auto;
+}
+</style>

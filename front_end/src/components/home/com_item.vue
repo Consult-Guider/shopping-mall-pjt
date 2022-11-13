c5c9379fb069b76a108ad310d8632aa780e6069d
iksadNorth <iksadnorth@gmail.com>
2022-11-13 19:30:23 +0900

    #10 - com_items.vue 설계 1. 지정된 열 갯수만큼 나열하도록 설계. 2. 개별적인 아이템을 표현하는 컴포넌트인 com_item.vue를 설계.

---
+++ front_end/src/components/home/com_item.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-container class="t2b center">
+        <com_img class="box x-center size-limit" v-bind="data" />
+        <h5>{{ data.name }}</h5>
+        <h5>{{ `${$util.transPrice(data.price)}원` }}</h5>
+    </v-container>
+</template>
+
+<script>
+export default {
+    props: {
+        data_retained: Number,
+    },
+    data() {return {
+        data: {
+            src: "https://thumbnail6.coupangcdn.com/thumbnails/remote/160x160ex/image/vendor_inventory/2330/addbde5c0bf9d3c4962ed113d0d2f831aa7ed419b13dfbd77fbdd805c63f.jpg",
+            link: null,
+            name: "존재하지 않는 아이템",
+            price: 1234253,
+        }
+    }},
+}
+</script>
+
+<style scoped>
+.size-limit {
+    max-height: 175px;
+}
+</style>
---
+++ front_end/src/components/search/com_items.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-sheet class="fill">
+        <v-continer class="fill y-gap">
+            <v-row 
+                v-for="r of $util.range(caluated.rows)" :key="r"
+            >
+                <v-col 
+                    v-for="c of $util.range(cols)" :key="c"
+                    class="y-gap"
+                >
+                    <com_item :data="getItem(1, r, c)" />
+                </v-col>
+            </v-row>
+        </v-continer>
+    </v-sheet>
+</template>
+
+<script>
+export default {
+    props: {
+        cols: {
+            type: Number,
+            default: 5,
+        },
+    },
+    data() {return {
+        data: {
+            items: [1,2,3,4,5,6,7,8,9,10,11,12,13],
+            mockItem: null
+        },
+        caluated: {
+            rows: [],
+            items: [],
+        }
+    }},
+    methods: {
+        setPagesNItems() {
+            this.caluated.rows = this.$util.getPages(1, this.cols, this.data.items);
+            this.caluated.items = this.$util.makeFrame(this.cols, this.data.items, this.data.mockItem);
+        },
+        getItem(page, row, col) {
+            return this.caluated.items[this.$util.getIdx(page, row, col, this.rows, this.cols)]
+        },
+    },
+    mounted() {
+        this.setPagesNItems();
+    },
+}
+</script>
+
+<style scopde>
+
+</style>

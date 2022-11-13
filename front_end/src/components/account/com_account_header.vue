907631fc0142cda2edbf71bb2737398ee453506c
iksadNorth <iksadnorth@gmail.com>
2022-11-13 22:00:28 +0900

    #13 - com_account_header.vue 설계 1. 배송 준비중, 배송 중, 배송 완료 갯수를 화면에 띄우도록 설계.

---
+++ front_end/src/components/account/com_account_header.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-card
+        variant="flat"
+    >
+        <div class="left2right">
+            <v-sheet class="size-limit text-white bg-blue-darken-1">
+                <h3 class="text-a-center">MY 쿠팡</h3>
+            </v-sheet>
+            <v-sheet class="size-limit text-white bg-blue-lighten-1">
+                <div class="fill y-gap">
+                    <h5 class="text-center">배송 준비중</h5>
+                    
+                    <h5 class="text-center">{{numReady}}개</h5>
+                </div>
+            </v-sheet>
+            <v-sheet class="size-limit text-white bg-blue-lighten-1">
+                <div class="fill y-gap">
+                    <h5 class="text-center">배송 중</h5>
+                    
+                    <h5 class="text-center">{{numIng}}개</h5>
+                </div>
+            </v-sheet>
+            <v-sheet class="size-limit text-white bg-blue-lighten-1">
+                <div class="fill y-gap">
+                    <h5 class="text-center">배송 완료</h5>
+                    
+                    <h5 class="text-center">{{numDone}}개</h5>
+                </div>
+            </v-sheet>
+        </div>
+    </v-card>
+</template>
+
+<script>
+export default {
+props: {
+    numReady: Number,
+    numIng: Number,
+    numDone: Number,
+},
+}
+</script>
+
+<style scoped>
+.size-limit {
+    width: 200px;
+    height: 100px;
+}
+.left2right {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+</style>

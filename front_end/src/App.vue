aa46b31b38c2127450dc3ce28d1db37e354c9c3d
iksadNorth <iksadnorth@gmail.com>
2022-11-13 11:49:20 +0900

    #5 - com_header.vue 생성 1. com_header를 생성해 헤더에 해당하는 컴포넌트 생성. 2. com_header.vue에서 메뉴 토글 버튼을 누르면 네비게이션 바가 열리게 App.vue에 이벤트를 emit시킴 3. 카테고리 버튼에 해당하는 컴포넌트 com_cartegory 생성. 4. 로고에 해당하는 컴포넌트 img_logo 생성. 5. 검색창에 해당하는 컴포넌트 search_window 생성. 6. 각종 버튼에 해당하는 컴포넌트 com_tools 생성. 7. com_footer를 생성해 footer에 해당하는 컴포넌트 생성.

--- front_end/src/App.vue
+++ front_end/src/App.vue
@@ -1,40 +1,124 @@
 <template>
   <v-app>
     <v-layout>
-      <v-app-bar color="header bg-black">
-        <Header />
+      <v-app-bar>
+        <com_header class="header" @clickCartegory="toggleMenu" />
       </v-app-bar>
 
+      <v-navigation-drawer 
+        class="bg-grey" permanent
+        v-model="drawer"
+      ></v-navigation-drawer>
+  
       <v-main>
         <router-view />
       </v-main>
     </v-layout>
 
-    <v-footer class="footer bg-black">
-      <Footer />
+    <v-footer class="footer">
+      <com_footer />
     </v-footer>
   </v-app>
 </template>
 
 <script>
-import Header from '@/components/com_header.vue'
-import Footer from '@/components/com_footer.vue'
-
 export default {
   name: 'App',
-
-  components: {
-    Header, Footer
-  },
-
   data: () => ({
-    //
+    drawer: false,
   }),
+  methods: {
+    toggleMenu() {
+      this.drawer = !this.drawer;
+    },
+  },
 }
 </script>
 
 <style>
-  .footer {
-    max-height: 10vh;
-  }
+.footer {
+  background-color: black;
+  max-height: 15vh;
+}
+.header {
+  max-height: 15vh;
+}
+
+/* 전역 CSS */
+/* 마우스 아이콘 변경 */
+.clickable {
+  cursor: pointer;
+}
+/* 반응형 정사각형 유지 */
+.box {
+  position: relative;
+  width: 100%;
+}
+.box::after {
+  display: block;
+  content: "";
+  padding-bottom: 100%;
+}
+/* 위아래로 나열 */
+.t2b {
+  display: flex;
+  flex-direction: column;
+}
+/* 좌우로 나열 */
+.l2r {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.l2r > * {
+  margin-right: 5px;
+}
+/* 위아래 같은 비율로 간격 벌리기 */
+.y-gap {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+/* 좌우 가운데 정렬 */
+.x-center {
+    margin: auto;
+}
+.child-x-center > .v-row {
+  display: flex;
+  justify-content: center;
+}
+/* 위아래 가운데 정렬 */
+.y-center {
+    display: flex;
+    align-items: center;
+}
+/* 꽉 채우기 */
+.fill {
+    height: 100%;
+    width: 100%;
+}
+/* 테두리 선 그리기 */
+.border-line {
+  border: 1px solid black;
+}
+.text-a-center {
+  text-align: center;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+/* 디버깅용 */
+.v-container {
+  /* margin-right: 2px; */
+  /* background-color: rgb(30, 255, 0); */
+}
+.v-row {
+  /* margin-right: 2px; */
+  /* background-color: rgb(255, 0, 0); */
+}
+.v-col {
+  /* margin-right: 2px; */
+  /* background-color: blue; */
+}
 </style>

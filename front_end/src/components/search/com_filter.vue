b23713cb9e13ec565ed8b816de9287ed13e05fd8
iksadNorth <iksadnorth@gmail.com>
2022-11-13 19:22:44 +0900

    #10 - com_filter.vue 설계 1. 필터 그룹 이름과 각 필터의 label, value가 주어지면 선택가능한 필터들을 생성하게 만듦 2. 옵션을 체크할 때마다 체크된 옵션들의 상태를 filters라는 변수에 저장하게 끔 설계

---
+++ front_end/src/components/search/com_filter.vue
@@ -0,0 +1,73 @@
+<template>
+    <v-card class="size-limit overflow-y-auto" variant="outlined">
+        <group_checkbox 
+            v-for="option of filter_options" :key="option"
+            v-bind="option"
+            @changeOptions="(obj) => updateOptions(obj)"
+        />
+    </v-card>
+</template>
+
+<script>
+export default {
+data() {return {
+    filters: new Map(),
+
+    filter_options: [
+        {
+            name: "생활 용품",
+            options: [
+                {
+                    'label': "컴퓨터", 
+                    'value': "computer", 
+                    'default': 'false',
+                },
+                {
+                    'label': "셔츠", 
+                    'value': "shirt", 
+                    'default': 'false',
+                },
+                {
+                    'label': "포장지", 
+                    'value': "wrapper", 
+                    'default': 'false',
+                },
+            ],
+        },
+        {
+            name: "컴퓨터 용품",
+            options: [
+                {
+                    'label': "GPU", 
+                    'value': "gpu", 
+                    'default': 'false',
+                },
+                {
+                    'label': "CPU", 
+                    'value': "cpu", 
+                    'default': 'false',
+                },
+                {
+                    'label': "RAM", 
+                    'value': "ram", 
+                    'default': 'false',
+                },
+            ],
+        },
+    ],
+    
+}},
+methods: {
+    updateOptions(obj) {
+        this.filters = new Map([...this.filters, ...obj]);
+    },
+},
+}
+</script>
+
+<style scoped>
+.size-limit {
+    height: 700px;
+    width: 200px;
+}
+</style>

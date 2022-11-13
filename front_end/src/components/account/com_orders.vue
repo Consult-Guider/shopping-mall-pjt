8ee5443b32475599210cd2aa8fcd82ea291ccbd4
iksadNorth <iksadnorth@gmail.com>
2022-11-13 22:43:38 +0900

    #13 - com_orders.vue 설계 1. 탭 형태로 '전체', '배송 준비중', '배송 중', '배송 완료'로 나눠 해당 형태의 주문 내역을 조회토록 함. 2. 주문 내역은 주문 아이템 이름, 썸네일, 가격, 갯수를 나열케 했음. 3. '배송조회', '반품 신청', '리뷰 작성', '판매자 문의' 버튼을 나열.

---
+++ front_end/src/components/account/com_orders.vue
@@ -0,0 +1,62 @@
+<template>
+    <v-card>
+        <v-btn-toggle
+            v-model="tab"
+            color="deep-purple-accent-3" rounded="0"
+            group
+        >
+            <v-btn value="total">
+                전체
+            </v-btn>
+
+            <v-btn value="ready">
+                배송 준비중
+            </v-btn>
+
+            <v-btn value="ing">
+                배송 중
+            </v-btn>
+
+            <v-btn value="done">
+                배송 완료
+            </v-btn>
+        </v-btn-toggle>
+
+        <v-card-text class="border-line">
+            <com_orders_unit 
+                v-for="item of items" :key="item"
+                v-bind="item" class="mb-6"
+            />
+        </v-card-text>
+
+        <!-- 페이지네이션 바 -->
+        <v-pagination
+        total-visible="5"
+        v-model="page"
+        :length="total"
+        ></v-pagination>
+    </v-card>
+</template>
+
+<script>
+export default {
+data() {return {
+    page: 1,
+    total: 100,
+
+    tab: "total",
+
+    items: [
+        {orderedAt: 20220205, state: "ing", name: "에이먼 먼지 제거제", price: 12300, num: 2, src: "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/1c8a/b38f3a8042198fd731c7f32d1355890b5f85e4214a7c9b1cbf2c068497ef.jpg"},
+        {orderedAt: 20220205, state: "ready", name: "에이먼 먼지 제거제", price: 12300, num: 2, src: "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/1c8a/b38f3a8042198fd731c7f32d1355890b5f85e4214a7c9b1cbf2c068497ef.jpg"},
+        {orderedAt: 20220205, state: "done", name: "에이먼 먼지 제거제", price: 12300, num: 2, src: "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/1c8a/b38f3a8042198fd731c7f32d1355890b5f85e4214a7c9b1cbf2c068497ef.jpg"},
+    ],
+}},
+}
+</script>
+
+<style scoped>
+.v-btn {
+    width: 100px;
+}
+</style>
---
+++ front_end/src/components/account/com_orders_unit.vue
@@ -0,0 +1,116 @@
+<template>
+    <v-card>
+        <v-card-text>
+        <p class="text-h6 text--primary">
+            {{ $util.str2date(orderedAt) }} 주문
+        </p>
+        <v-card>
+            <!-- 사진, 이름, 가격, 갯수 -->
+            <div class="t2b">
+                <h2 class="mt-6 ml-6">{{ transState(state) }}</h2>
+                <div class="l2r">
+                    <com_img class="img-size-limit" no_link :src="src" />
+                    <div class="t2b">
+                        <p>{{ name }}</p>
+                        <div class="l2r">
+                            <p class="text-grey mr-6">{{ $util.transPrice(price) }} 원</p>
+                            <p class="text-grey mr-6">{{ num }} 개</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </v-card>
+        <v-card>
+            <!-- 사진, 이름, 가격, 갯수 -->
+            <div class="l2r children-btn pa-3">
+                <v-btn
+                    color="primary"
+                    @click="btnListUp.click"
+                >{{ btnListUp.label }}</v-btn>
+
+                <v-btn
+                    color="primary"
+                    @click="btnExchange.click"
+                >{{ btnExchange.label }}</v-btn>
+
+                <v-btn
+                    color="primary"
+                    @click="btnReview.click"
+                >{{ btnReview.label }}</v-btn>
+
+                <v-btn
+                    color="primary"
+                    @click="btnQuery.click"
+                >{{ btnQuery.label }}</v-btn>
+            </div>
+        </v-card>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default {
+props: {
+    orderedAt: Number, 
+    state: String, 
+    name: String, 
+    price: Number, 
+    num: Number,
+    src: String,
+},
+data() {return {
+    btnListUp: {
+        label: "배송조회",
+        click: this.onClickListUp,
+    },
+    btnExchange: {
+        label: "교환, 반품 신청",
+        click: this.onClickExchange,
+    },
+    btnReview: {
+        label: "리뷰 작성하기",
+        click: this.onClickReview,
+    },
+    btnQuery: {
+        label: "판매자 문의",
+        click: this.onClickQuery,
+    },
+}},
+methods: {
+    transState(state) {
+        if(state=="ready") {
+            return "배송 준비중";
+        } else if(state=="ing") {
+            return "배송 중";
+        } else if(state=="done") {
+            return "배송 완료";
+        } else {
+            return "상태 이상";
+        }
+    },
+    onClickListUp() {
+        console.log("click onClickListUp");
+    },
+    onClickExchange() {
+        console.log("click onClickExchange");
+    },
+    onClickReview() {
+        console.log("click onClickReview");
+    },
+    onClickQuery() {
+        console.log("click onClickQuery");
+    },
+},
+}
+</script>
+
+<style scoped>
+.img-size-limit {
+    width: 125px;
+}
+.children-btn > .v-btn {
+    width: 100px;
+
+    font-size: x-small;
+}
+</style>

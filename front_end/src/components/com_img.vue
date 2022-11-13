28c10bf9a0cfaa1e1751935e681c5f775094ac74
iksadNorth <iksadnorth@gmail.com>
2022-11-13 17:55:13 +0900

    #4 - com_display.vue 생성. 1. 카테고리의 이름을 표기하는 컴포넌트인 category_name.vue의 위치와 해당 카테고리에 포함되어 있는 아이템을 전시하는 컴포넌트인 category_items.vue의 위치를 설정함. 2. category_name.vue와 category_items.vue를 생성. 3. category_items.vue 설계 시, 지정한 열 갯수에 의해 나오는 page 수 계산할 필요가 생기는데, 이를 위해 util.js를 추가로 생성.

---
+++ front_end/src/components/com_img.vue
@@ -0,0 +1,38 @@
+<template>
+    <img 
+        class="clickable"
+        :src="Src" 
+        @click="onClickAD"
+    />
+</template>
+
+<script>
+export default {
+props: {
+    src: String,
+    link: String,
+    no_link: {
+        type: Boolean,
+        default: false,
+    },
+},
+computed: {
+    Src() {return this.src ?? this.$defaults.image;},
+    Link() {return this.link ?? this.$defaults.link;},
+},
+methods: {
+    onClickAD() {
+        console.log(`click onClickAD. ${this.Link}`);
+        if(!this.no_link) {
+            this.$router.push(this.Link);
+        }
+    },
+},
+}
+</script>
+
+<style scoped>
+    img {
+        object-fit: contain;
+    }
+</style>

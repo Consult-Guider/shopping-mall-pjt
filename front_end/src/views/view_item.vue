24369c1780ca5e0c7e621ecc2ae6d142d0713aff
iksadNorth <iksadnorth@gmail.com>
2022-11-13 19:42:40 +0900

    #11 - view_item.vue 설계 1. 상품 정보를 나타내는 컴포넌트인 com_item_info.vue 위치 조정 2. 상품과 관련된 다른 상품을 보여주는 컴포넌트인 com_recommend.vue 위치 조정 3. 탭 형식으로 상품의 설명을 담당하는 부분인 com_desc.vue와 리뷰 작성 및 확인 담당 컴포넌트인 com_review.vue와 문의를 담당하는 컴포넌트인 com_query.vue, 교환과 관련된 정보를 담당하는 com_exchange를 포함하고 있다.

---
+++ front_end/src/views/view_item.vue
@@ -0,0 +1,67 @@
+<template>
+<v-container>
+<v-row>
+<v-col>
+    <com_item_info />
+</v-col>
+</v-row>
+
+<v-row>
+<v-col>
+    <com_recommend />
+</v-col>
+</v-row>
+
+<v-row>
+<v-col>
+    <v-card>
+        <v-tabs
+            v-model="tab"
+            bg-color="grey"
+            fixed-tabs
+        >
+        <v-tab value="desc">상품 상세</v-tab>
+        <v-tab value="review">상품평</v-tab>
+        <v-tab value="query">상품 문의</v-tab>
+        <v-tab value="exchange">배송/교환/반품 안내</v-tab>
+        </v-tabs>
+
+        <v-card-text>                    
+            <v-window v-model="tab">
+                <v-window-item value="desc">
+                    <com_desc />
+                </v-window-item>
+
+                <v-window-item value="review">
+                    <com_review />
+                </v-window-item>
+
+                <v-window-item value="query">
+                    <com_query />
+                </v-window-item>
+
+                <v-window-item value="exchange">
+                    <com_exchange />
+                </v-window-item>
+            </v-window>
+        </v-card-text>
+    </v-card>
+</v-col>
+</v-row>
+</v-container>
+</template>
+
+<script>
+export default {
+computed: {
+    iid() {return this.$route.params.iid;},
+},
+data() {return {
+    tab: "desc",
+}},
+}
+</script>
+
+<style scoped>
+
+</style>

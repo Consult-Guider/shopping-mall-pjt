3c31a6d8ed370d392478e09ca09deb99eab2143a
iksadNorth <iksadnorth@gmail.com>
2022-11-13 20:18:13 +0900

    #11 - com_review.vue 설계 1. 프로필, 이름, 상품평을 나열하되 이름은 익명화하여 게시함. 2. 날짜 역시 백엔드 서버에서 온 데이터 그대로를 사용하지 않고 수정하여 사용함. 3. 구매 당시 선택한 옵션에 대해서도 표기. 4. 평가 내용에 대해서 나열 5. 추천 비추천 버튼도 나열

---
+++ front_end/src/components/item/com_desc.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="t2b">
+        <com_img 
+            v-for="img of images" :key="img"
+            class="fill my-2 size-limit" 
+            :src="img" no_link
+        />
+    </div>
+</template>
+
+<script>
+export default {
+data() {return {
+    images: [
+        null,
+        null,
+        null,
+    ],
+}},
+}
+</script>
+
+<style scoped>
+.size-limit {
+    max-height: 400px;
+}
+</style>

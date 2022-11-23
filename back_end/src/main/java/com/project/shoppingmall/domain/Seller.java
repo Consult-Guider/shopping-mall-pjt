689e4497a7550391f2a47f6375f8a4e9d0bc1a7e
iksadNorth <iksadnorth@gmail.com>
2022-11-23 17:03:27 +0900

    #40 - User, Admin, Seller entity 수정, DTO 생성.
    자세한 것은 https://github.com/Consult-Guider/shopping-mall-pjt/issues/40 에 상세히 담겨 있다.

--- back_end/src/main/java/com/project/shoppingmall/domain/Seller.java
+++ back_end/src/main/java/com/project/shoppingmall/domain/Seller.java
@@ -1,53 +1,31 @@
 package com.project.shoppingmall.domain;
 
-import com.project.shoppingmall.type.RoleType;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Table;
 
+@ToString
+// 위는 테스트 편의를 위한 어노테이션
 @EntityListeners(AuditingEntityListener.class)
 // 위는 JpaAuditing을 위한 어노테이션
-@SQLDelete(sql = "UPDATE `account` SET deleted_at = NOW() WHERE id = ?")
+@SQLDelete(sql = "UPDATE `seller` SET deleted_at = NOW() WHERE id = ?")
 @Where(clause = "deleted_at IS NULL")
 // 위는 SOFT DELETE을 위한 어노테이션
-@Table(name = "account")
+@Table(name = "seller")
 @Entity
 @NoArgsConstructor @Getter @Setter
-@AllArgsConstructor @Builder
 // 위는 JPA Entity 사용을 위한 어노테이션
-public class Seller implements UserDetails {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private LocalDateTime createdAt;
-    private LocalDateTime deletedAt;
-
-    private String email;
-    private String password;
+public class Seller extends LoginEntity {
     private String name;
     private String companyName;
     private String phoneNum;
     private String address;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        SimpleGrantedAuthority roleUser = new SimpleGrantedAuthority(RoleType.SELLER.getName());
-        return List.of(roleUser);
-    }
-
-    @Override
-    public String getUsername() { return email; }
-
-    @Override public boolean isAccountNonExpired() { return deletedAt==null; }
-    @Override public boolean isAccountNonLocked() { return deletedAt==null; }
-    @Override public boolean isCredentialsNonExpired() { return deletedAt==null; }
-    @Override public boolean isEnabled() { return deletedAt==null; }
 }

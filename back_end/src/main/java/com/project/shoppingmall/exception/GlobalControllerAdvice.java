acdf1789813f078749ea289d24a01c642e892e69
iksadNorth <iksadnorth@gmail.com>
2022-11-25 18:19:59 +0900

    #45 - 로그인 시, 필수 항목을 기입하지 않아도 그대로 로그인을 수행하는 버그 처리 1. build.gradle에 spring-boot-starter-validation 적용하기. 2. AuthController.java의 doLogin함수 파라미터에 @Valid 적용하여 Validation 처리하게 유도. 3. LoginRequest.java의 각 칼럼에 @NotNull 부여한 뒤, email 칼럼에 특별히 @Email 부여. 4. GlobalControllerAdvice.java에 MethodArgumentNotValidException 예외 처리 추가.
    자세한 것은 https://github.com/Consult-Guider/shopping-mall-pjt/issues/45에 상세히 담겨 있다.

--- back_end/src/main/java/com/project/shoppingmall/exception/GlobalControllerAdvice.java
+++ back_end/src/main/java/com/project/shoppingmall/exception/GlobalControllerAdvice.java
@@ -2,7 +2,9 @@ package com.project.shoppingmall.exception;
 
 import com.project.shoppingmall.model.response.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,5 +16,11 @@ public class GlobalControllerAdvice {
     public ResponseEntity<?> errorHandler(AuthenticationException e) {
         return ResponseEntity.status(e.getErrorCode().getStatus())
                 .body(Response.error(e.printErrorMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> errorHandler(MethodArgumentNotValidException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(Response.error(e.getMessage()));
     }
 }

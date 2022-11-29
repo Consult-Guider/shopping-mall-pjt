2493d7bc03683a4658012a1a83b1cf6a85b162ec
iksadNorth <iksadnorth@gmail.com>
2022-11-29 23:34:15 +0900

    #52 - AccessDeniedHandler와 AccessDeniedHandler 에러 메세지에도 통일성을 부여.

--- back_end/src/main/java/com/project/shoppingmall/type/ErrorCode.java
+++ back_end/src/main/java/com/project/shoppingmall/type/ErrorCode.java
@@ -15,7 +15,9 @@ public enum ErrorCode {
     ACCOUNT_NOT_FOUNDED(HttpStatus.NOT_FOUND, "해당 계정은 찾을 수가 없습니다."),
     WRONG_PASSWORD(HttpStatus.UNAUTHORIZED, "로그인을 시도한 비밀번호가 맞지 않습니다."),
     INVALID_ROLETYPE(HttpStatus.BAD_REQUEST, "해당 RoleType은 존재하지 않습니다."),
-    ACCOUNT_ALREADY_EXISTED(HttpStatus.CONFLICT, "해당 계정은 이미 존재합니다.");
+    ACCOUNT_ALREADY_EXISTED(HttpStatus.CONFLICT, "해당 계정은 이미 존재합니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN,  "해당 계정은 접근 불가 상태입니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "로그인 후 사용 가능합니다. ");
 
     private final HttpStatus status;
     private final String message;

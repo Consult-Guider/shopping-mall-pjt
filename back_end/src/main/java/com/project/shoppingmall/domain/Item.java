c9d20cfe3b44afa24363f945ee2c0edba0f7ce4e
iksadNorth <iksadnorth@gmail.com>
2022-12-03 13:24:43 +0900

    #36 -  Item 인덱스 설계 1. Item에 필요한 칼럼들을 정의함. [name, price, seller, imagePath] 2. Item에 저장될 seller는 SellerDto로 다운 캐스팅 가능한 객체를 받을 수 있게 설정. 3. 해당 엔티티에 들어갈 수 있는 모든 내장 객체를 nested라는 디렉토리 아래에 설정함.

---
+++ back_end/src/main/java/com/project/shoppingmall/domain/Item.java
@@ -0,0 +1,61 @@
+package com.project.shoppingmall.domain;
+
+import com.project.shoppingmall.domain.nested.*;
+import com.project.shoppingmall.model.LoginDto;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@ToString
+@Document(indexName = "item")
+// 위는 Elastic Search Document 사용을 위한 어노테이션
+@NoArgsConstructor @Getter @Setter
+// 위는 JPA Entity 사용을 위한 어노테이션
+// TODO: 해당 엔티티는 JPA를 통해 delete가 이뤄지는 것이 아니기 때문에 Soft Delete를 직접 구현해서 사용해야 함.
+@AllArgsConstructor @Builder
+public class Item {
+    @Id @Field(type = FieldType.Auto)
+    private String id;
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second_millis) @CreatedDate
+    private LocalDateTime createdAt;
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second_millis)
+    private LocalDateTime deletedAt;
+
+    @Field(type = FieldType.Text)
+    private String name;
+    @Field(type = FieldType.Long)
+    private Long price;
+    @Field(type = FieldType.Long) @CreatedBy
+    private LoginDto seller;
+    @Field(type = FieldType.Text)
+    private String imagePath;
+
+    @Field(type = FieldType.Object) @Builder.Default
+    private List<Image> imageList = new ArrayList<>();
+    @Field(type = FieldType.Object) @Builder.Default
+    private List<Option> optionList = new ArrayList<>();
+    @Field(type = FieldType.Object) @Builder.Default
+    private List<Description> descriptionList = new ArrayList<>();
+    @Field(type = FieldType.Object) @Builder.Default
+    private List<Review> reviewList = new ArrayList<>();
+    @Field(type = FieldType.Object) @Builder.Default
+    private List<Question> questionList = new ArrayList<>();
+    @Field(type = FieldType.Object) @Builder.Default
+    private List<Tag> tagList = new ArrayList<>();
+
+    public void addImageList(Image obj) { imageList.add(obj); }
+    public void addOptionList(Option obj) { optionList.add(obj); }
+    public void addDescriptionList(Description obj) { descriptionList.add(obj); }
+    public void addReviewList(Review obj) { reviewList.add(obj); }
+    public void addQuestionList(Question obj) { questionList.add(obj); }
+    public void addTagList(Tag obj) { tagList.add(obj); }
+}

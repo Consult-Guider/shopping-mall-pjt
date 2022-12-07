06ccd2ca03dd2bfaaa1a36b178248d0fc87735d0
iksadNorth <iksadnorth@gmail.com>
2022-12-07 18:02:07 +0900

    #15 - 우측 문의 내역 열거 컴포넌트(com_query_chat) 설계 1. 부모 컴포넌트에서 qid를 지정하지 않으면 chat 변수는 빈 Array값이 되고 따라서 선택된 문의 내역이 없음을 알림. 2. 부모 컴포넌트에서 qid를 받으면 원래대로 라면 axios를 이용해 백엔드로 쿼리를 보내야 함. 일단은 해당 메소드만 지정. 3. 해당 채팅 내용을 보여주기 위한 메세지 단위 컴포넌트(com_query_chat_unit)을 추가 설계. 4. 답변 여부를 판단하는 isAnswer메소드를 설계하고 이에 따라 배경색을 흰색으로 할지 회색으로 할지 결정함.

---
+++ front_end/src/components/historyQuery/com_query_chat.vue
@@ -0,0 +1,75 @@
+<template>
+    <v-container v-if="(chats.length)">
+        <v-row v-for="chat in chats" :key="chat">
+            <v-col>
+                <com_query_chat_unit v-bind="chat" />
+            </v-col>
+        </v-row>
+    </v-container>
+    <v-container v-else>
+        <v-row>
+            <v-col>
+                <h1 class="text-center">선택된 문의 내역이 없습니다.</h1>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    props: {
+        qid: Number,
+    },
+    data() {return {
+        chats: [
+            // {
+            //     state: "Qusetion",
+            //     content: "문의 관련 내용 쏼라 쏼라",
+            //     createdAt: 20220105,
+            // },
+            // {
+            //     state: "Answer",
+            //     content: "문의 관련 내용 답변 쏼라 쏼라",
+            //     createdAt: 20220106,
+            // },
+            // {
+            //     state: "Qusetion",
+            //     content: "문의 관련 내용 쏼라 쏼라",
+            //     createdAt: 20220105,
+            // },
+            // {
+            //     state: "Answer",
+            //     content: "문의 관련 내용 답변 쏼라 쏼라",
+            //     createdAt: 20220106,
+            // },
+            // {
+            //     state: "Qusetion",
+            //     content: "문의 관련 내용 쏼라 쏼라",
+            //     createdAt: 20220105,
+            // },
+            // {
+            //     state: "Answer",
+            //     content: "문의 관련 내용 답변 쏼라 쏼라",
+            //     createdAt: 20220106,
+            // },
+        ],
+    }},
+    methods: {
+        fetchQueryChat: function(qid) {
+            console.log("call fetchQueryChat: " + qid);
+            // TODO: 여기에 qid를 이용해서 axios로 백엔드에 챗팅 내역을 요구함. 나중에 구현하기.
+        },
+    },
+    watch: {
+        qid(val) {
+            if(val) {
+                this.fetchQueryChat(val);
+            }
+        },
+    },
+}
+</script>
+
+<style scoped>
+
+</style>

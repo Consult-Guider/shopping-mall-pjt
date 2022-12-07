0a1b0fa80211fe61438c39c5e45400aeed771b74
iksadNorth <iksadnorth@gmail.com>
2022-12-07 17:51:55 +0900

    #15 - 상품 문의 내역 주된 내용 정의 1. com_history_query.vue에서 좌측에는 선택가능한 문의 내역 목록들을 띄우고 우측에는 선택한 문의 채팅 내역을 띄운다. 2. 두 부분 모두 최대 높이는 500px로 설정 후, 크기 이상의 자식 컴포넌트가 발생하면 스크롤로 움직일 수 있게 구성함. 3. 선택된 문의 내역의 배경색을 회색으로 설정함.

---
+++ front_end/src/components/historyQuery/com_history_query.vue
@@ -0,0 +1,57 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="5">
+                <v-card 
+                elevation="5" 
+                class="outer-shell overflow-auto"
+                >
+                    <v-item-group>
+                        <v-item 
+                        v-for="item in items" :key="item"
+                        v-slot="{ isSelected, toggle }"
+                        >
+                            <v-card
+                            :color="isSelected ? 'grey-lighten-2' : ''" @click="toggle"
+                            class="d-flex align-center pa-3" 
+                            >
+                                <com_query_state v-bind=item :isSelected=isSelected @changeChat="onChangeChat" />
+                            </v-card>
+                        </v-item>
+                    </v-item-group>
+                </v-card>
+            </v-col>
+            <v-col>
+                <v-card 
+                elevation="5" 
+                class="outer-shell overflow-auto"
+                >
+                    <com_query_chat :qid="qid" />
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    props: {
+        items: Array,
+    },
+    data() {return {
+        qid: null,
+    }},
+    methods: {
+        onChangeChat(qid) {
+            console.log("call onChangeChat" + ` ${qid}`);
+            this.qid = qid;
+        },
+    },
+}
+</script>
+
+<style scoped>
+    .outer-shell {
+        max-height: 500px;
+    }
+</style>

0eb0472fdb3a2b50b8551743a78af8209b048219
iksadNorth <iksadnorth@gmail.com>
2022-12-13 17:39:03 +0900

    #77 - 상품평 작성란 생성 1. 상품 전시 페이지에 상품평 작성란 생성.

---
+++ front_end/src/components/item/com_review_posting.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-divider class="my-2"/>
+
+    <v-card variant="outlined">
+        <v-card-title>상품평 달기</v-card-title>
+        <v-rating v-model="rating" />
+        <v-card-text>{{rating}}점</v-card-text>
+        <com_input_comment @post="postingEventHandler" />
+    </v-card>
+
+    <v-divider class="my-2"/>
+</template>
+
+<script>
+export default {
+data() {
+    return {
+        rating: 3,
+    }
+},
+methods: {
+    postingEventHandler(text) {
+        console.log("call postingEventHandler: " + text);
+
+        // TODO: text 데이터값을 받은 후 이를 리뷰 내역에 반영.[axios 이용해서 새로 생성.]
+        if(text) this.$emit("update");
+    },
+},
+}
+</script>
+
+<style scoped>
+
+</style>

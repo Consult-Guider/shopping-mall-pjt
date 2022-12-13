e0dba36b285b847c75254e66f49f673797f8e19f
iksadNorth <iksadnorth@gmail.com>
2022-12-13 23:18:05 +0900

    #77 - 리뷰 내역 관련 컴포넌트 생성.

---
+++ front_end/src/components/historyReview/com_history_review.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col>
+                <!-- 상품평 -->
+                <v-card height="500" class="overflow-y-auto">
+                    <div v-for="review of reviews" :key="review">
+                        <div class="l2r">
+                            <com_review_unit v-bind="review" />
+                            <v-spacer/>
+                            <v-btn
+                            @click="linkTo(review.iid)"
+                            >해당 상품 페이지로</v-btn>
+                        </div>
+                        <v-divider class="my-3" />
+                    </div>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+props: {
+    reviews: Array,
+},
+methods: {
+    linkTo(iid) {
+        this.$router.push(this.$endPoint.item(iid));
+    },
+},
+}
+</script>
+
+<style scoped>
+
+</style>

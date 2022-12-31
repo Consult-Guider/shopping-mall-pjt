bec1308eb08df28f0d4b91de9c4fb964cc152de6
iksadNorth <iksadnorth@gmail.com>
2022-12-31 13:53:00 +0900

    #90 - 이름 생성 규칙에 관한 클래스 생성 1. 파일 이름을 보다 쉽게 다룰 수 있게 하는 FilenameUtils 의존성 추가 2. 이미지 파일 이름을 생성하는 클래스인 S3FileNameManager 생성. 3. S3FileNameManager 작동을 확인하는 단위테스트 생성. 4. S3FileManagerTest 관련한 사소한 수정 사항 추가. [ 설명을 위한 주석 등등 추가. ]

---
+++ back_end/src/test/java/com/project/shoppingmall/utils/fileManager/S3FileNameManagerTest.java
@@ -0,0 +1,69 @@
+package com.project.shoppingmall.utils.fileManager;
+
+import com.project.shoppingmall.utils.SetProfile;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@SetProfile
+class S3FileNameManagerTest {
+    @Test
+    @DisplayName("[정상 작동][extractExtension] multipartfile에서 확장자 추출.")
+    public void givenMultipart_whenCallExtractExtension_thenReturnExtension() throws IOException {
+        // given
+        URL url = new URL("https://cdn.pixabay.com/photo/2016/01/18/19/42/fractal-1147253_960_720.jpg");
+
+        String originalFilenameWithoutExtension = "originalFilename";
+        String extensionExpected = "jpg";
+        String originalFilename = String.join(".", originalFilenameWithoutExtension, extensionExpected);
+
+        String extensionCalculated;
+
+        //  when
+        try (InputStream inputStream = url.openStream()) {
+            MockMultipartFile image = new MockMultipartFile("image", originalFilename, null, inputStream);
+            extensionCalculated = S3FileNameManager.extractExtension(image);
+        }
+
+        //  then
+        log.debug("Expected:\t{}", extensionExpected);
+        log.debug("Calculated:\t{}", extensionCalculated);
+        assertThat(extensionCalculated).isEqualTo(extensionExpected);
+    }
+
+    @Test
+    @DisplayName("[정상 작동][makeFileName] S3에 저장 시, 사용할 파일 이름 정하는 함수 호출.")
+    public void givenMultipartAndWords_whenCallMakeFileName_thenReturnFileName() throws IOException {
+        // given
+        URL url = new URL("https://cdn.pixabay.com/photo/2016/01/18/19/42/fractal-1147253_960_720.jpg");
+        String originalFilenameWithoutExtension = "originalFilename";
+        String extensionExpected = "jpg";
+        String originalFilename = String.join(".", originalFilenameWithoutExtension, extensionExpected);
+
+        String fileDir1 = "one";
+        String fileDir2 = "two";
+
+        String root = S3FileNameManager.ROOT_DIR;
+        String fileNameExpected = String.join("/", root, fileDir1, fileDir2) + "." + extensionExpected;
+        String fileNameCalculated;
+
+        //  when
+        try (InputStream inputStream = url.openStream()) {
+            MockMultipartFile image = new MockMultipartFile("mock name", originalFilename, null, inputStream);
+            fileNameCalculated = S3FileNameManager.makeFileName(image, fileDir1, fileDir2);
+        }
+
+        //  then
+        log.debug("Expected:\t{}", fileNameExpected);
+        log.debug("Calculated:\t{}", fileNameCalculated);
+        assertThat(fileNameCalculated).isEqualTo(fileNameExpected);
+    }
+}

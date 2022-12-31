f9b766aac997995bf773f63a0d579f87d41006a0
iksadNorth <iksadnorth@gmail.com>
2022-12-31 14:37:44 +0900

    #94 - axios에 baseURL를 전역적으로 설정하기. 1. 환경 설정값에 관한 내용을 @/cfg/env.js에 담아두고 이를 전역적으로 선언하기 위해 @/cfg/index.js에 설정함. 2. @/axios/index.js에 baseURL을 설정함.

--- front_end/src/cfg/index.js
+++ front_end/src/cfg/index.js
@@ -1,10 +1,11 @@
+import env from "@/cfg/env";
 import endPoint from "@/cfg/endPoint";
 import defaults from "@/cfg/defaults";
 import util from "@/utils/util";
 
 export default {
     install(Vue) {
-        Vue.config.globalProperties.$ServerUrl = 'http://localhost:8080';
+        Vue.config.globalProperties.$env = env;
 
         Vue.config.globalProperties.$endPoint = endPoint;
 

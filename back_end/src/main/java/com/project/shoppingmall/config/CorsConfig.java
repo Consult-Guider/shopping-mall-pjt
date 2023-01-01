1681a326122577541bd61a310c99ab1612845ea9
iksadNorth <iksadnorth@gmail.com>
2023-01-01 17:11:03 +0900

    #98 - CorsConfig Bean 충돌 버그 수정

--- back_end/src/main/java/com/project/shoppingmall/config/CorsConfig.java
+++ back_end/src/main/java/com/project/shoppingmall/config/CorsConfig.java
@@ -4,14 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
-@EnableWebSecurity
 @Configuration
 @RequiredArgsConstructor
 public class CorsConfig {
@@ -19,7 +17,7 @@ public class CorsConfig {
     String[] allowedUrls;
 
     @Bean
-    public CorsConfigurationSource corsConfig() {
+    public CorsConfigurationSource configurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
         Arrays.stream(allowedUrls).forEach(config::addAllowedOrigin);

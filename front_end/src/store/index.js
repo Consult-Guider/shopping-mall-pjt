bd38d45c1b770e1e56bc08f9b2385d4e0f847813
iksadNorth <iksadnorth@gmail.com>
2023-01-02 11:35:06 +0900

    #100 - vuex를 이용한 유저 정보 저장 1. 로그인 시, accessToken을 decoding하여 정보를 취득 후, 해당 정보를 스토어에 저장 및 localStorage에 저장. 2. 로그 아웃 시, 유저 정보를 스토어에서 삭제하고  localStorage에서 삭제. 3. roleType도 변경이 자주 일어날 확률이 크므로 따로 클래스로 관리. 4. @/util/index.js에 JWT를 해독할 수 있는 함수 생성.

--- front_end/src/store/index.js
+++ front_end/src/store/index.js
@@ -1,12 +1,52 @@
 import { createStore } from 'vuex';
+import roleType from '@/utils/roleType';
+import util from '@/utils/util';
+import { DelAccessToken, saveAccessToken } from '@/utils/localStorage';
 
 export default createStore({
 	state: {
+		user: {
+			email: null,
+			role: roleType.roles.NULL,
+		},
 	},
 	getters: {
+		isLogin(state) {
+			return state.user.email != null;
+		},
+		isUser(state) {
+			return state.user.role == roleType.roles.USER;
+		},
+		isSeller(state) {
+			return state.user.role == roleType.roles.SELLER;
+		},
+		isAdmin(state) {
+			return state.user.role == roleType.roles.ADMIN;
+		},
 	},
 	mutations: {
+		login(state, token) {
+			// token Decoding.
+			const decoded = util.JWTDecode(token);
+
+			// 스토어에 저장.
+			state.user = {
+				email: decoded.email,
+				role: roleType.find(decoded.role),
+			};
+
+			// localStorage에 저장.
+			saveAccessToken(token);
+		},
+		logout(state) {
+			// 스토어에서 삭제.
+			state.user = {
+				email: null,
+				role: roleType.roles.NULL,
+			};
+
+			// localStorage에서 삭제.
+			DelAccessToken();
+		},
 	},
-	actions: {
-	}
 });

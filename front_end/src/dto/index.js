9f41efb5264486a11e29b0d4c19800df82c81d5d
iksadNorth <iksadnorth@gmail.com>
2023-01-04 12:36:27 +0900

    dto 소스코드에서 Response와 Request의 구역을 명확히 하여 가독성을 높힘.

--- front_end/src/dto/index.js
+++ front_end/src/dto/index.js
@@ -1,5 +1,7 @@
 import roleType from "@/utils/roleType";
 
+// Request
+
 export class authReq {
     constructor(role, email, password) {
         this.role = role;
@@ -17,44 +19,6 @@ export class authReq {
             email: this.email,
             password: this.password,
         };
-    }
-}
-
-export class BaseResponse {
-    constructor(response) {
-        this.response = response;
-    }
-
-    static of(response) {
-        return new this(response);
-    }
-
-    getData() {
-        return this.response.data;
-    }
-}
-
-export class authRes extends BaseResponse {
-    get token() {
-        return this.getData().token;
-    }
-}
-
-export class ErrRes {
-    constructor(response) {
-        this.response = response;
-    }
-
-    static of(response) {
-        return new this(response);
-    }
-
-    getData() {
-        return this.response.response.data;
-    }
-
-    get errorCode() {
-        return this.getData().errorCode;
     }
 }
 
@@ -101,4 +65,100 @@ export class SignUpReq {
                 throw new Error(`roleType의 형태가 옳바르지 못함. ${this.role}`);
         }
     }
+}
+
+export class UserUpdateReq {
+    constructor(role, params) {
+        this.role = role;
+        this.params = params;
+    }
+
+    static of(role, params) {
+        return new this(role, params);
+    }
+
+    data(attr) {
+        return this.params[attr];
+    }
+
+    json() {
+        switch (this.role) {
+            case roleType.roles.USER:
+                return {
+                    uid: this.data("id"),
+                    email: this.data("email"),
+                    password: this.data("password"),
+                    name: this.data("name"),
+                    phoneNum: this.data("phone"),
+                    address: this.data("address"),
+                };
+            case roleType.roles.SELLER:
+                return {
+                    uid: this.data("id"),
+                    email: this.data("email"),
+                    password: this.data("password"),
+                    name: this.data("name"),
+                    phone: this.data("phone"),
+                    companyName: this.data("companyName"),
+                    address: this.data("address"),
+                };
+            case roleType.roles.ADMIN:
+                return {
+                    uid: this.data("id"),
+                    email: this.data("email"),
+                    password: this.data("password"),
+                    name: this.data("name"),
+                    phone: this.data("phone"),
+                    address: this.data("address"),
+                };
+        
+            default:
+                throw new Error(`roleType의 형태가 옳바르지 못함. ${this.role}`);
+        }
+    }
+}
+
+// Response
+
+export class BaseResponse {
+    constructor(response) {
+        this.response = response;
+    }
+
+    static of(response) {
+        return new this(response);
+    }
+
+    getData() {
+        return this.response.data;
+    }
+}
+
+export class PageResponse extends BaseResponse {
+    getData() {
+        return this.response.data.data;
+    }
+}
+
+export class ErrRes extends BaseResponse {
+    getData() {
+        return this.response.response.data;
+    }
+
+    get errorCode() { return this.getData().errorCode; }
+}
+
+export class authRes extends BaseResponse {
+    get token() {
+        return this.getData().token;
+    }
+}
+
+export class LoginRes extends PageResponse {
+    get id() { return this.getData().id; }
+    get email() { return this.getData().email; }
+    get name() { return this.getData().name; }
+    get phoneNum() { return this.getData().phoneNum; }
+    get companyName() { return this.getData().companyName; }
+    get address() { return this.getData().address; }
 }

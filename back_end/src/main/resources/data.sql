bb878113cbfcd25ed18026e866faf81090d84ebf
iksadNorth <iksadnorth@gmail.com>
2023-01-06 20:44:07 +0900

    #116 - 광고 배너 도메인 설계 1. 추가적인 고려사항을 AdImg.java에 반영함. 2. data.sql에 해당 도메인의 예시 데이터를 추가.

--- back_end/src/main/resources/data.sql
+++ back_end/src/main/resources/data.sql
@@ -1,5 +1,7 @@
-INSERT INTO ad_img (`created_at`, `path`) VALUES (NOW(), 'test1');
-INSERT INTO ad_img (`created_at`,`path`) VALUES (NOW(), 'test2');
+INSERT INTO ad_img (`created_at`, `item_name`, `company_name`, `path`, `link`, `start_at`, `end_at`) VALUES (NOW(), '맥심 모카 골드', 'Maxim', 'https://cdn.pixabay.com/photo/2022/11/15/04/54/automotive-7593064_960_720.jpg', 'https://www.coupang.com/vp/products/4550236145?itemId=9821768243&vendorItemId=71030128009&pickType=COU_PICK', '2023-01-01', '2023-01-05');
+INSERT INTO ad_img (`created_at`, `item_name`, `company_name`, `path`, `link`, `start_at`, `end_at`) VALUES (NOW(), '맥심 모카 레드', 'Midim', 'https://cdn.pixabay.com/photo/2022/11/15/04/54/automotive-7593064_960_720.jpg', 'https://www.coupang.com/vp/products/4550236145?itemId=9821768243&vendorItemId=71030128009&pickType=COU_PICK', '2023-01-26', '2023-01-31');
+INSERT INTO ad_img (`created_at`, `item_name`, `company_name`, `path`, `link`, `start_at`, `end_at`) VALUES (NOW(), '맥심 모카 블루', 'Minim', 'https://cdn.pixabay.com/photo/2022/11/15/04/54/automotive-7593064_960_720.jpg', 'https://www.coupang.com/vp/products/4550236145?itemId=9821768243&vendorItemId=71030128009&pickType=COU_PICK', '2023-01-22', '2023-01-29');
+INSERT INTO ad_img (`created_at`, `item_name`, `company_name`, `path`, `link`, `start_at`, `end_at`) VALUES (NOW(), '맥심 모카 다크', 'Maximum', 'https://cdn.pixabay.com/photo/2022/11/15/04/54/automotive-7593064_960_720.jpg', 'https://www.coupang.com/vp/products/4550236145?itemId=9821768243&vendorItemId=71030128009&pickType=COU_PICK', '2123-01-22', '2123-01-29');
 
 INSERT INTO USER_ACCOUNT (`created_at`,`email`,`name`,`password`,`phone_num`) VALUES (NOW(),'iksadnorth@gmail.com','박정훈','q1w2e3r4','010-1234-5678');
 

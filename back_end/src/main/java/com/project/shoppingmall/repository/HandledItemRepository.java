d0a144476538ab48529c0cca2211adff455cfeaf
iksadNorth <iksadnorth@gmail.com>
2023-02-04 12:59:12 +0900

    #146 - 해당 기능 구현에서 불필요한 부분 제거

--- back_end/src/main/java/com/project/shoppingmall/repository/HandledItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.shoppingmall.repository;
-
-import com.project.shoppingmall.domain.HandledItem;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface HandledItemRepository extends
-        ElasticsearchRepository<HandledItem, String>,
-        KeywordRepository<HandledItem> {
-}

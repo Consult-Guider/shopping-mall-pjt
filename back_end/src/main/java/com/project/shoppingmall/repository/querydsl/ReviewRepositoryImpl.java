a979464c28190653e206163eff7ebf139248a8ee
junmo95 <97028441+junmo95@users.noreply.github.com>
2023-02-08 17:06:51 +0900

    #158-repository, domain 추가 작업 Service 구현을 위한 repository, domain추가및 변경

--- back_end/src/main/java/com/project/shoppingmall/repository/querydsl/ReviewRepositoryImpl.java
+++ back_end/src/main/java/com/project/shoppingmall/repository/querydsl/ReviewRepositoryImpl.java
@@ -2,30 +2,28 @@ package com.project.shoppingmall.repository.querydsl;
 
 import com.project.shoppingmall.domain.Review;
 import com.project.shoppingmall.repository.CustomReviewRepository;
-import lombok.RequiredArgsConstructor;
+import com.project.shoppingmall.utils.ElasticSearchOperationsUtil;
 import org.elasticsearch.index.query.MatchQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.metrics.Avg;
 import org.elasticsearch.search.aggregations.metrics.AvgAggregationBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.elasticsearch.core.ElasticsearchAggregations;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.SearchHit;
-import org.springframework.data.elasticsearch.core.SearchHits;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 
-import java.util.List;
-import java.util.Optional;
+import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
+import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
 
-import static org.elasticsearch.index.query.QueryBuilders.*;
-
-@RequiredArgsConstructor
 public class ReviewRepositoryImpl implements CustomReviewRepository {
-    private final ElasticsearchOperations elasticsearchClient;
+    private final ElasticSearchOperationsUtil<Review> fetchUtil;
+
+    @Autowired
+    public ReviewRepositoryImpl(ElasticsearchOperations elasticsearchClient) {
+        this.fetchUtil = new ElasticSearchOperationsUtil<>(Review.class, elasticsearchClient);
+    }
 
     private MatchQueryBuilder queryForFindWithItemId(String iid) {
         return matchQuery("item.id", iid);
@@ -41,72 +39,28 @@ public class ReviewRepositoryImpl implements CustomReviewRepository {
     public Page<Review> findReviewByItemId(String iid, Pageable pageable) {
         QueryBuilder query = queryForFindWithItemId(iid);
 
-        SearchHits<Review> result = elasticsearchClient.search(
-                new NativeSearchQueryBuilder()
-                        .withQuery(query)
-                        .withPageable(pageable)
-                        .build(),
-                Review.class
-        );
-
-        List<Review> items = result.getSearchHits().stream()
-                .map(SearchHit::getContent)
-                .toList();
-        return new PageImpl<>(items, pageable, result.getTotalHits());
+        return fetchUtil.fetchWithPageable(query, pageable);
     }
 
     @Override
     public Page<Review> findReviewByUserId(Long uid, Pageable pageable) {
         QueryBuilder query = queryForFindWithUserId(uid);
 
-        SearchHits<Review> result = elasticsearchClient.search(
-                new NativeSearchQueryBuilder()
-                        .withQuery(query)
-                        .withPageable(pageable)
-                        .build(),
-                Review.class
-        );
-
-        List<Review> items = result.getSearchHits().stream()
-                .map(SearchHit::getContent)
-                .toList();
-        return new PageImpl<>(items, pageable, result.getTotalHits());
+        return fetchUtil.fetchWithPageable(query, pageable);
     }
 
     @Override
     public Page<Review> findReviewBySellerId(Long uid, Pageable pageable) {
         QueryBuilder query = queryForFindWithSellerId(uid);
 
-        SearchHits<Review> result = elasticsearchClient.search(
-                new NativeSearchQueryBuilder()
-                        .withQuery(query)
-                        .withPageable(pageable)
-                        .build(),
-                Review.class
-        );
-
-        List<Review> items = result.getSearchHits().stream()
-                .map(SearchHit::getContent)
-                .toList();
-        return new PageImpl<>(items, pageable, result.getTotalHits());
+        return fetchUtil.fetchWithPageable(query, pageable);
     }
 
     @Override
     public Page<Review> searchReviewByKeyword(String keyword, Pageable pageable) {
         QueryBuilder query = boolQuery().should(QueryBuilders.matchQuery("content", keyword));
 
-        SearchHits<Review> result = elasticsearchClient.search(
-                new NativeSearchQueryBuilder()
-                        .withQuery(query)
-                        .withPageable(pageable)
-                        .build(),
-                Review.class
-        );
-
-        List<Review> items = result.getSearchHits().stream()
-                .map(SearchHit::getContent)
-                .toList();
-        return new PageImpl<>(items, pageable, result.getTotalHits());
+        return fetchUtil.fetchWithPageable(query, pageable);
     }
 
     @Override
@@ -114,21 +68,10 @@ public class ReviewRepositoryImpl implements CustomReviewRepository {
         String AggName = "AVG";
         AvgAggregationBuilder aggregation = AggregationBuilders.avg(AggName).field("rating");
 
-        SearchHits<Review> result = elasticsearchClient.search(
-                new NativeSearchQueryBuilder()
-                        .withQuery(queryForFindWithItemId(iid))
-                        .withAggregations(aggregation)
-                        .build(),
-                Review.class
-        );
-
-        return Optional.of(result)
-                .map(SearchHits::getAggregations)
-                .map(ElasticsearchAggregations.class::cast)
-                .map(ElasticsearchAggregations::aggregations)
-                .map(aggregations -> aggregations.get(AggName))
-                .map(Avg.class::cast)
+        return fetchUtil.fetchAggregation(queryForFindWithItemId(iid), Avg.class, AggName, aggregation)
                 .map(Avg::getValue)
+                .filter(d -> !Double.isNaN(d))
+                .filter(d -> !Double.isInfinite(d))
                 .orElse(0d);
     }
 }
